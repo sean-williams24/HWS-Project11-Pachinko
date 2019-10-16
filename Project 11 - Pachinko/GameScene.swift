6f1d0317dfe89c4f68734435cf353b4dc33d5a07
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-10-16 13:35:10 +0100

    Feat: add bouncing balls and fixed ball in position

--- Project 11 - Pachinko/GameScene.swift
+++ Project 11 - Pachinko/GameScene.swift
@@ -21,6 +21,13 @@ class GameScene: SKScene {
         addChild(background)
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = CGPoint(x: 512, y: 0)
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
+        // False means it will collide with other things but is fixed in position
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
        
     }
     
@@ -28,10 +35,19 @@ class GameScene: SKScene {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
         
-        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
-        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
-        box.position = location
-        addChild(box)
+        let ball = SKSpriteNode(imageNamed: "ballRed")
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+        //Bouncyness
+        ball.physicsBody?.restitution = 0.4
+        ball.position = location
+        addChild(ball)
+        
+        
+        
+//        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+//        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+//        box.position = location
+//        addChild(box)
     }
 
 }
