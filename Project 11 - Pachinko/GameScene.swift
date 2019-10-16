cc0a73759637f2f94ae53c9077d1909408ff831a
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-10-16 13:17:52 +0100

    feat: add image assists and set background jpeg. Create squares and set them to drop to floor when green is tapped by getting first touch

--- Project 11 - Pachinko/GameScene.swift
+++ Project 11 - Pachinko/GameScene.swift
@@ -12,10 +12,26 @@ class GameScene: SKScene {
 
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        // ignore alpha values
+        background.blendMode = .replace
+        //Draw behind everything else
+        background.zPosition = -1
+        addChild(background)
         
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
        
     }
     
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        
+        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+        box.position = location
+        addChild(box)
+    }
 
 }
