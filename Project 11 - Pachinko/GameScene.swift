e7c238ebb22afc1bb9ea276a46f68a14d5c5779c
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2019-10-17 09:23:38 +0100

    FEAT: create edit label and place on screen. Find out what node is being tapped when screen is touched. If editing label then toggle editing mode. If in editing mode create a random shaped box on screen, if not then create a ball.

--- Project 11 - Pachinko/GameScene.swift
+++ Project 11 - Pachinko/GameScene.swift
@@ -11,10 +11,19 @@ import SpriteKit
 class GameScene: SKScene, SKPhysicsContactDelegate {
 
     var scoreLabel: SKLabelNode!
-    
     var score = 0 {
         didSet {
             scoreLabel.text = "Score: \(score)"
+        }
+    }
+    var editLabel: SKLabelNode!
+    var editingMode: Bool = false {
+        didSet {
+            if editingMode {
+                editLabel.text = "Done"
+            } else {
+                editLabel.text = "Edit"
+            }
         }
     }
     
@@ -32,6 +41,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         scoreLabel.horizontalAlignmentMode = .right
         scoreLabel.position = CGPoint(x: 980, y: 700)
         addChild(scoreLabel)
+        
+        editLabel = SKLabelNode(fontNamed: "Chalkduster")
+        editLabel.text = "Edit"
+        editLabel.position = CGPoint(x: 80, y: 70)
+        addChild(editLabel)
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         physicsWorld.contactDelegate = self
@@ -55,19 +69,36 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
         
-        let ball = SKSpriteNode(imageNamed: "ballRed")
-        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
-        //Bouncyness
-        ball.physicsBody?.restitution = 0.4
-        
-        //Bounce off of everthing and please tell me about every single bounce/collision
-        ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
-        
-        ball.position = location
-        ball.name = "ball"
-        addChild(ball)
-        
-        
+        //Find out what object is being tapped
+        let objects = nodes(at: location)
+        if objects.contains(editLabel) {
+            editingMode.toggle()
+        } else {
+            if editingMode {
+                // Create a box
+                let size = CGSize(width: Int.random(in: 16...128), height: 16)
+                let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1), size: size)
+                box.zRotation = CGFloat.random(in: 0...3)
+                box.position = location
+
+                box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
+                box.physicsBody?.isDynamic = false
+
+                addChild(box)
+            } else {
+                let ball = SKSpriteNode(imageNamed: "ballRed")
+                ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+                //Bouncyness
+                ball.physicsBody?.restitution = 0.4
+                
+                //Bounce off of everthing and please tell me about every single bounce/collision
+                ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
+                
+                ball.position = location
+                ball.name = "ball"
+                addChild(ball)
+            }
+        }
         
 //        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
 //        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
